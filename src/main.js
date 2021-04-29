c87d97c262fa520da782e3ed39bd6f5ffbbbb01d
luipalencia <luipalencia>
2021-04-29 08:57:05 -0400

    development of unit tests for HU1, HU2 and HU3.

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 // Este es el punto de entrada de tu aplicacion
 import { changeRoute } from './lib/router.js';
+import { observer } from './lib/firebase/firebase-auth.js';
 
 const init = () => {
   changeRoute(window.location.hash);
+  observer();
   window.addEventListener('hashchange', () => {
     changeRoute(window.location.hash);
   });
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,16 +1,56 @@
 // importamos la funcion que vamos a testear
-import { myFunction } from '../src/lib/index.js';
 import { logIn } from '../src/lib/views/templateLogIn.js';
+import { signIn } from '../src/lib/views/templateSignIn.js';
+import { error } from '../src/lib/views/templateError.js';
+import { recoverPassword } from '../src/lib/views/templatePassword.js';
+import { home } from '../src/lib/views/templateHome.js';
 
-describe('myFunction', () => {
-  it('debería ser una función', () => {
-    expect(typeof myFunction).toBe('function');
+describe('logIn', () => {
+  const divLogIn = logIn();
+  it('should be a function', () => {
+    expect(typeof logIn).toBe('function');
+  });
+  it('should render without crashing', () => {
+    expect(divLogIn instanceof HTMLElement).toBe(true);
   });
 });
 
-describe('logIn', () => {
+describe('signIn', () => {
+  const divSignIn = signIn();
+  it('should be a function', () => {
+    expect(typeof signIn).toBe('function');
+  });
   it('should render without crashing', () => {
-    const divLogIn = logIn();
-    expect(divLogIn instanceof HTMLElement).toBe(true);
+    expect(divSignIn instanceof HTMLElement).toBe(true);
+  });
+});
+
+describe('error', () => {
+  const divError = error();
+  it('should be a function', () => {
+    expect(typeof error).toBe('function');
+  });
+  it('should render without crashing', () => {
+    expect(divError instanceof HTMLElement).toBe(true);
+  });
+});
+
+describe('recoverPassword', () => {
+  const divRecoverPassword = recoverPassword();
+  it('should be a function', () => {
+    expect(typeof recoverPassword).toBe('function');
+  });
+  it('should render without crashing', () => {
+    expect(divRecoverPassword instanceof HTMLElement).toBe(true);
+  });
+});
+
+describe('home', () => {
+  const divHome = home();
+  it('should be a function', () => {
+    expect(typeof home).toBe('function');
+  });
+  it('should render without crashing', () => {
+    expect(divHome instanceof HTMLElement).toBe(true);
   });
 });
